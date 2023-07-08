d3b574e28d92598e18194a85dcbf90df31c902a4
Paris Pettis <pettis_paris@hotmail.com>
2023-07-08 16:42:21 +0300

    added post dates

--- main_blog/views.py
+++ main_blog/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-post_date']   
 
 class ArticleView(DetailView):
     model = Post
