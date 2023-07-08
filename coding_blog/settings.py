6fa0c00f96e8d549da77904ad5ae997e2f821ebb
Paris Pettis <pettis_paris@hotmail.com>
2023-07-08 18:43:12 +0300

    added user authentication
    user register/login pages for account creation and login.
    user authentication required for content to show in certain pages.

--- coding_blog/settings.py
+++ coding_blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_blog'
+    'main_blog',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- coding_blog/urls.py
+++ coding_blog/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_blog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
