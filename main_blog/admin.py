e241b63913bb23341476496c96a41d84dd701c4c
Paris Pettis <pettis_paris@hotmail.com>
2023-07-30 15:25:27 +0300

    feature/ADD-007-added-user-profile
    added user profile info on edit profile and on user's post.

--- main_blog/admin.py
+++ main_blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Profile
 
 # Register your models here.
 admin.site.register(Post) # Post entries accessible from the admin area.
 admin.site.register(Category) # Category entries accessible from the admin area.
+admin.site.register(Profile) # Profile entries accessible from the admin area
--- main_blog/models.py
+++ main_blog/models.py
@@ -12,6 +12,18 @@ class Category(models.Model):
     
     def get_absolute_url(self):
         return reverse('home')
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    github_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
